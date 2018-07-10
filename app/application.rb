729148f5b0486ea1523ef1d21e9d8cb3cc6143b5
Marissa Zhong <mwz2101@columbia.edu>
2018-07-10 17:34:36 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,15 @@
 class Application
-  
+
+  @@items = ["Apples", "Carrots", "Pears"]
+  @@cart = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match()
+
+    resp.finish
+  end
+
 end
