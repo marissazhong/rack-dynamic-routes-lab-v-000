3b278ca66e7ee3c95e4aa008da8bb768a2b0493a
Marissa Zhong <mwz2101@columbia.edu>
2018-07-10 17:40:43 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,16 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = @@items.find
+      item = @@items.find{|i| i.name == item_name}
+      if item
+        resp.write item.price
+      else
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
     resp.finish
   end
 end
